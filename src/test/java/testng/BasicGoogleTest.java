8b5f8896da076ef3d976d047af01b632ad7bf0e9
Melad <melad.alamri@gmail.com>
2024-03-11 09:52:58 +0300

    add testBasic on testng package. trying testng

---
+++ src/test/java/testng/BasicGoogleTest.java
@@ -0,0 +1,12 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class BasicGoogleTest extends Tests{
+
+    @Test
+    public  void quickGoogleSearch(){
+
+    }
+
+}
---
+++ src/test/java/testng/Tests.java
@@ -0,0 +1,53 @@
+package testng;
+
+import engine.ActionBot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class Tests {
+
+    protected WebDriver driver;
+    protected Wait<WebDriver> wait;
+    public static Logger logger;
+
+    public ActionBot bot;
+
+    //   = LogManager.getLogger(Tests.class.getName());
+    @BeforeClass
+    public static void beforeAll() {
+        Configurator.initialize(null, "src/main/resources/properties/log4j2.properties");
+        logger = LogManager.getLogger(junit.Tests.class.getName());
+    }
+
+
+    @BeforeMethod
+    public void beforeEach() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        logger.info("Opening Chrome Browser");
+        driver = new ChromeDriver(chromeOptions);
+
+        logger.info("Configuring 5 second explicit wait");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        bot = new ActionBot(driver, wait, logger);
+
+    }
+
+    @AfterMethod
+    public void afterEach() {
+        logger.info("Quitting Browser");
+        driver.quit();
+    }
+}
