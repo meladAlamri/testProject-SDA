4c6a7c488532183f64a917161b6cae05d210fb47
Melad <melad.alamri@gmail.com>
2024-03-13 12:14:39 +0300

    adding xml parameters

--- src/test/java/tests/testng/Tests.java
+++ src/test/java/tests/testng/Tests.java
@@ -11,11 +11,14 @@ import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -31,24 +34,33 @@ public class Tests {
     protected ActionBot bot;
 
     protected static JSONObject testData;
+
     @BeforeClass
     public static void beforeAll() throws IOException, ParseException {
         Configurator.initialize(null, "src/main/resources/properties/log4j2.properties");
         logger = LogManager.getLogger(Tests.class.getName());
-        testData =  (JSONObject) new JSONParser().parse( new FileReader("src/test/resources/testData/sample.json", StandardCharsets.UTF_8) );
+        testData = (JSONObject) new JSONParser().parse(new FileReader("src/test/resources/testData/sample.json", StandardCharsets.UTF_8));
     }
 
 
+    @Parameters("option")
     @BeforeMethod
-    public void beforeEach() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("start-maximized");
-        logger.info("Opening Chrome Browser");
-        driver = new ChromeDriver(chromeOptions);
+    public void beforeEach(String option) {
+        logger.info("Opening " + option + " Browser");
+        switch (option) {
+            case "Chrome" -> driver = new ChromeDriver();
+            case "Edge" -> driver = new EdgeDriver();
+            default -> driver = new FirefoxDriver();
+        }
+        driver.manage().window().maximize();
 
         logger.info("Configuring 5 second explicit wait");
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        bot = new ActionBot(driver, wait, logger);
+        wait = new
+
+                WebDriverWait(driver, Duration.ofSeconds(5));
+        bot = new
+
+                ActionBot(driver, wait, logger);
 
     }
 
