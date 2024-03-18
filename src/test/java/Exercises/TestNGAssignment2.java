84d9449bdf7c09a81dacde520332a9cdd6ed9e47
Melad <melad.alamri@gmail.com>
2024-03-18 13:25:46 +0300

    Update Assignment 2: adding data provider to take data from Excel sheet

--- src/test/java/Exercises/TestNGAssignment2.java
+++ src/test/java/Exercises/TestNGAssignment2.java
@@ -5,10 +5,12 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+
 public class TestNGAssignment2 extends Tests {
     /**
      * Go to <a href="https://testpages.eviltester.com/styled/apps/notes/simplenotes.html">Test page</a>(done)
@@ -16,29 +18,24 @@ public class TestNGAssignment2 extends Tests {
      * <br>Run it with three different browsers using XML file(done)
      * <br>Run it parallel with three threads(done)
      */
-    @Test
-    public void Assignment2() throws IOException {
+    @Test(dataProvider = "data-provider")
+    public void Assignment2(String title, String note) {
 //        Go to https://testpages.eviltester.com/styled/apps/notes/simplenotes.html
         bot.navigate("https://testpages.eviltester.com/styled/apps/notes/simplenotes.html");
 
-//        Add 10 notes using data provider with Excel
-        String path = System.getProperty("user.home") + "/IdeaProjects/testProject/src/test/resources/testData/Book2.xlsx";
-        Workbook workbook = new XSSFWorkbook(new FileInputStream(path));
 
-        Sheet sheet = workbook.getSheetAt(0);
         By titleLocator = By.id("note-title-input");
         By notesLocator = By.id("note-details-input");
         By submitLocator = By.id("add-note");
         By listLocator = By.className("note-in-list");
-        for (int i = 0; i < 10; i++) {
-            bot.type(titleLocator, sheet.getRow(i).getCell(0).toString());
-            bot.type(notesLocator, sheet.getRow(i).getCell(1).toString());
-            bot.click(submitLocator);
 
-        }
-        int listSize = driver.findElements(listLocator).size();
-        System.out.println("The list notes size is: " + listSize);
-        Assert.assertEquals(listSize,10);
+        bot.type(titleLocator, title);
+        bot.type(notesLocator, note);
+        bot.click(submitLocator);
+
+        boolean check = driver.findElement(listLocator).isDisplayed();
+        System.out.println("The list notes is displayed: " + check);
+        Assert.assertTrue(check);
 
 
 //        Run it with 3 different browsers using XML file
@@ -47,6 +44,20 @@ public class TestNGAssignment2 extends Tests {
 
     }
 
+    @DataProvider(name = "data-provider")
+    public Object[][] getData() throws IOException {
+        Object[][] data = new Object[10][2];
+        //        Add 10 notes using data provider with Excel
+        String path = System.getProperty("user.home") + "/IdeaProjects/testProject/src/test/resources/testData/Book2.xlsx";
+        Workbook workbook = new XSSFWorkbook(new FileInputStream(path));
 
+        Sheet sheet = workbook.getSheetAt(0);
+        for (int i = 0; i < 10; i++) {
+            data[i][0] = sheet.getRow(i).getCell(0).toString();
+            data[i][1] = sheet.getRow(i).getCell(1).toString();
+
+        }
+        return data;
+    }
 
 }
