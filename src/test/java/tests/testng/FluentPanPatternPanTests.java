ded164139675d744634559154d6165fcffcee779
Melad <melad.alamri@gmail.com>
2024-03-19 12:11:15 +0300

    practices fluent pattern page object model using "practice.expandtesting.com" website

---
+++ src/test/java/tests/testng/FluentPanPatternPanTests.java
@@ -0,0 +1,47 @@
+package tests.testng;
+
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.expandTesting.Login;
+
+public class FluentPanPatternPanTests extends Tests{
+    /**
+     * breakout task: 40 minutes
+     * - implement fluent POM design, using abstract page class
+     * - add a new failed login test
+     * - read data from json
+     * - generate single allure html file
+     */
+
+    @Test
+    public void successLoginTest(){
+        JSONObject dataInfo = (JSONObject) testData.get("PracticeDomeLoginTestData");
+        dataInfo =(JSONObject) dataInfo.get("case1");
+        String actionText = new Login(driver,bot)
+                .goTo()
+                .login(dataInfo.get("Username").toString(), dataInfo.get("Password").toString())
+                .readMassage();
+        Assert.assertEquals(actionText,"You logged into a secure area!");
+    }
+    @Test
+    public void filedLoginPasswordTest(){
+        JSONObject dataInfo = (JSONObject) testData.get("PracticeDomeLoginTestData");
+        dataInfo =(JSONObject) dataInfo.get("case2");
+        String actionText = new Login(driver,bot)
+                .goTo()
+                .login(dataInfo.get("Username").toString(), dataInfo.get("Password").toString())
+                .readMassage();
+        Assert.assertEquals(actionText,"Your password is invalid!");
+    }
+    @Test
+    public void filedLoginUserNameTest(){
+        JSONObject dataInfo = (JSONObject) testData.get("PracticeDomeLoginTestData");
+        dataInfo =(JSONObject) dataInfo.get("case3");
+        String actionText = new Login(driver,bot)
+                .goTo()
+                .login(dataInfo.get("Username").toString(), dataInfo.get("Password").toString())
+                .readMassage();
+        Assert.assertEquals(actionText,"Your username is invalid!");
+    }
+}
