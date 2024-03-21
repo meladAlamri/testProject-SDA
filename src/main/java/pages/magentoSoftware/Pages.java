583d2c1551349118040d1c86f674cbb047a388d2
Melad <melad.alamri@gmail.com>
2024-03-21 12:22:03 +0300

    General page imputation for Luma web test

---
+++ src/main/java/pages/magentoSoftware/Pages.java
@@ -0,0 +1,15 @@
+package pages.magentoSoftware;
+
+import engine.ActionBot;
+import org.openqa.selenium.WebDriver;
+
+public abstract class Pages {
+    final WebDriver driver;
+
+     final ActionBot bot;
+
+    public Pages(WebDriver driver, ActionBot bot){
+        this.driver = driver;
+        this.bot = bot;
+    }
+}
